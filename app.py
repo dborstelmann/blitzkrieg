bfa27fc9c0c74f6fb6e2e6a764984823d0f3e28d
Daniel Borstelmann <dborstelmann@gmail.com>
2015-11-09 21:10:26 -0600

    updates for instagram

--- app.py
+++ app.py
@@ -5,6 +5,21 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
+@app.route('/instagram_redirect', methods=['GET'])
+def instagram_redirect():
+    r = requests.post("https://api.instagram.com/oauth/access_token",
+        data = {
+            "client_id":"e7896c47f26e463ba097362492257dd7",
+            "client_secret":"2db4eb3d197e4feea21505aa2a1dcf50",
+            "grant_type":"authorization_code",
+            "redirect_uri":"https://blitzkrieg-pulse.herokuapp.com/instagram_redirect",
+            "code":request.GET['code'],
+        }
+    )
+
+    print(r)
+    return render_template('hello.html')
+
 # When debug is true python server will update on file saves instead of needing
 # to re-run server
 if __name__ == '__main__':
--- database_setup.sql
+++ database_setup.sql
@@ -4,3 +4,10 @@ CREATE TABLE "user" (
     "email" varchar(50) NOT NULL,
     "password" varchar(50) NOT NULL
 );
+
+CREATE TABLE "instagram_user" (
+    "id" serial PRIMARY KEY,
+    "user_id" serial NOT NULL,
+    "username" varchar(50) NOT NULL,
+    "full_name" varchar(50) NOT NULL
+);
